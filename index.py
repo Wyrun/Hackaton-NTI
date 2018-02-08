04cd05f9cf983bdea27858d63beac74e7792e2cd
Wyrun <32933287+Wyrun@users.noreply.github.com>
2018-02-08 14:16:06 +0700

    FinallyWorks

--- index.py
+++ index.py
@@ -14,10 +14,10 @@ DataX = []
 for i in Data:
     if i[0] == "R":
         people[2] += 1
-        DataY.append(float(1))
+        DataY.append(float(2))
     elif i[0] == "L":
         people[0] += 1
-        DataY.append(float(0.5))
+        DataY.append(float(1))
     else:
         people[1] += 1
         DataY.append(float(0))
@@ -28,7 +28,8 @@ print(people)
 
 plt.plot(people)
 plt.ylabel('Psycological Health')
-#plt.show()
+#plt.line2d.drawstyle('steps')
+plt.show()
 """
 from sklearn.neural_network import MLPClassifier
 clf = MLPClassifier(solver='lbfgs', activation='relu',
@@ -36,11 +37,16 @@ hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit([[0., 1.], [0., 0.]], [1., 0.])
 #clf.predict([['5', '5', '5', '5']])
 clf.predict([['1', '1']])"""
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(DataX, DataY, test_size=0.2, random_state=42)
+
+
+
 from sklearn.neural_network import MLPClassifier
 X = [[5.0, 5.0, 5.0, 3.0], [5.0, 5.0, 5.0, 4.0]]
 Y = [1., 1.]
 clf = MLPClassifier(solver='lbfgs', activation='relu',
 hidden_layer_sizes=(4, 8, 4, 4, 4))
-clf.fit(DataX, DataY)
+clf.fit(X_train, Y_train)
 print("Hekko Workt")
 print(clf.predict([[2., 2., 1., 0.1]]))
